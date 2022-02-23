14c4c0aea6e2fdc534823bcbc4f2eb80f31127ad
LuisMm16 <97635446+LuisMm16@users.noreply.github.com>
2022-02-23 09:32:38 -0500

    Sesion3

---
+++ Sesion3/Ejercicio2.js
@@ -0,0 +1,18 @@
+for(let i = 1; i<=10; i++){
+  switch(i){
+    case 2:
+      console.log("Número primo");
+      break;
+    case 3:
+      console.log("Número primo");
+      break;
+    case 5:
+      console.log("Número primo");
+      break;
+  	case 7:
+      console.log("Número primo");
+      break;
+    default:
+      console.log("El número de la iteración es " + i);
+  }
+}
