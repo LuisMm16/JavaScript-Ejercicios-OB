1de9df48d2b2c3db3ad9343e85c2af2f7392ae1a
LuisMm16 <97635446+LuisMm16@users.noreply.github.com>
2022-02-23 09:39:51 -0500

    Sesion4

---
+++ Sesion4/Ejercicio1.js
@@ -0,0 +1,4 @@
+let myStr = "Andrea está muy triste";
+console.log("Inicialmente tenemos: " + myStr);
+let myStrModified = myStr.replace("triste", "feliz");
+console.log("Despúes del replace: " + myStrModified);
